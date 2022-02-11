af25080ebf0d15346b6c1f7cbc0b3ab1b38c67f2
emonmorol <ekramulhasane69@gmail.com>
2022-02-12 00:24:28 +0600

    error handled

--- JS/money.js
+++ JS/money.js
@@ -10,18 +10,22 @@ function updateInitailInput(initialTotal,initialAmount){
     const totalValue = document.getElementById(initialTotal);
     const previusAmountText = totalValue.innerText;
     const previousAmount = parseFloat(previusAmountText);
-    totalValue.innerText = previousAmount + initialAmount;
+    if( initialAmount > 0 ){
+        totalValue.innerText = previousAmount + initialAmount;
+    }
 }
 
 function updateTotalBalance(isAdd , inputAmount){
     const totalBalace = document.getElementById('total-balance');
     const totalBalanceText = totalBalace.innerText;
     const previousBalance = parseFloat(totalBalanceText);
-    if(isAdd == true){
-        totalBalace.innerText = previousBalance + inputAmount;
-    }
-    else{
-        totalBalace.innerText = previousBalance - inputAmount;
+    if(inputAmount>0){
+        if(isAdd == true ){
+            totalBalace.innerText = previousBalance + inputAmount;
+        }
+        else{
+            totalBalace.innerText = previousBalance - inputAmount;
+        }
     }
 }
 
